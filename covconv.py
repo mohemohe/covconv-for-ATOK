bbe733389a2dd2a97439b8dc04b3c7065276c188
mohemohe <mohemohe@ghippos.net>
2014-05-12 15:25:57 +0900

    no message

---
+++ covconv.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+# covconv.py
+# こふ語ダイレクト for ATOK
+# Copyright (c) 2014 mohemohe
+#
+# this code licensed under MIT/X11 License.
+# please visit ' http://opensource.org/licenses/mit-license.php '.
+__version__ = 1
+
+import urllib
+from xml.etree.ElementTree import *
+
+def atok_plugin_run_process( request_data ):
+  result_data = {}
+  candidate_array = []
+
+  covconv_pack = tryCov( request_data['composition_string'].encode('utf-8') )
+
+  if covconv_pack['success'].decode('utf-8') == '1':
+    candidate_array.append( {'hyoki' : covconv_pack['covlang_kana'].rstrip('っ').decode('utf-8')} )
+    candidate_array.append( {'hyoki' : covconv_pack['covlang_kana'].decode('utf-8')} )
+    candidate_array.append( {'hyoki' : covconv_pack['covlang_orig'].rstrip('っ').decode('utf-8')} )
+    candidate_array.append( {'hyoki' : covconv_pack['covlang_orig'].decode('utf-8')} )
+
+  if int(covconv_pack['covconv4atok_version'].decode('utf-8')) > int(__version__):
+    candidate_array.append( { 'hyoki'             : u"こふ語ダイレクト for ATOK の更新があります" , 
+                              'alternative'       : u"http://ghippos.net/special/kovlang.php" , # あとでちゃんとページを作る
+                              'alternative_type'  : u"url_jump_string" } )
+
+  result_data['candidate'] = candidate_array
+
+  return result_data
+
+def tryCov( ja_JP ):
+  covconv_pack = {}
+
+  url = 'https://api.ghippos.net/covconv/atok/'
+  params = urllib.urlencode( {'ja_JP': ja_JP} )
+  xml = urllib.urlopen( url, params )
+
+  element = fromstring( xml.read() )
+
+  covconv_pack['success'] = element.findtext('.//success')
+  covconv_pack['covlang_kana'] = element.findtext('.//covlang_kana').encode('utf-8')
+  covconv_pack['covlang_orig'] = element.findtext('.//covlang_orig').encode('utf-8')
+  covconv_pack['covconv4atok_version'] = element.findtext('.//covconv4atok_version').encode('utf-8')
+
+  return covconv_pack
